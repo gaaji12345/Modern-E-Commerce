df25056bbf0bbba3d9280eae1d99c979bfe67eb3
gaji12345 <gajimax12345@gmail.com>
2024-09-08 21:25:52 +0530

    create enums

---
+++ src/main/java/com/gaaji/Gaaji/Eccomerce/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.gaaji.Gaaji.Eccomerce.enums;/*  gaajiCode
+    99
+    08/09/2024
+    */
+
+public enum OrderStatus {
+    PENDING, CONFIRMED, SHIPPED, DELIVERED, CANCELLED, RETURNED
+
+}
---
+++ src/main/java/com/gaaji/Gaaji/Eccomerce/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.gaaji.Gaaji.Eccomerce.enums;/*  gaajiCode
+    99
+    08/09/2024
+    */
+
+public enum UserRole {
+    ADMIN, USER
+}
