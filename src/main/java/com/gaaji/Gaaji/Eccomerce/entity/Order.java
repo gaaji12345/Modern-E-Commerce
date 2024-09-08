0d1e1a44612ff6256016bf96cabf925fb6573107
gaji12345 <gajimax12345@gmail.com>
2024-09-08 21:25:29 +0530

    entities done

---
+++ src/main/java/com/gaaji/Gaaji/Eccomerce/entity/Order.java
@@ -0,0 +1,28 @@
+package com.gaaji.Gaaji.Eccomerce.entity;/*  gaajiCode
+    99
+    08/09/2024
+    */
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalPrice;
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY,  cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItemList;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+}
---
+++ src/main/java/com/gaaji/Gaaji/Eccomerce/entity/Payment.java
@@ -0,0 +1,28 @@
+package com.gaaji.Gaaji.Eccomerce.entity;/*  gaajiCode
+    99
+    08/09/2024
+    */
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    private String method;
+    private String status;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private  Order order;
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
