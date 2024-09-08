0d3f7a6e92e9f7379003cfafde2fa14f548a984b
gaji12345 <gajimax12345@gmail.com>
2024-09-08 19:43:44 +0530

    test

---
+++ src/main/java/com/gaaji/Gaaji/Eccomerce/GaajiEccomerceApplication.java
@@ -0,0 +1,13 @@
+package com.gaaji.Gaaji.Eccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaajiEccomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaajiEccomerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaaji/Gaaji/Eccomerce/GaajiEccomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaaji.Gaaji.Eccomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaajiEccomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
